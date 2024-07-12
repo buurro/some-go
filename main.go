00dba308e3d31a3fe87ebe3353f0d53d255a9054
Marco Burro <marcoburro98@gmail.com>
2024-07-12 10:33:17 +0200

    test stuff

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
 
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
+
+type sessionState int
+
+const (
+	audioView   sessionState = iota
+	audioInput  sessionState = iota
+	audioOutput sessionState = iota
+	entryView
+)
+
+type MainModel struct {
+	state sessionState
+	entry tea.Model
+}
 
 var docStyle = lipgloss.NewStyle().Margin(1, 2)
 
@@ -47,35 +64,47 @@ func (m model) View() string {
 	return docStyle.Render(m.list.View())
 }
 
+type Node struct {
+	Id   int32
+	Type string
+	Info struct {
+		Props struct {
+			Node_nick   string `json:"node.nick"`
+			Media_class string `json:"media.class"`
+		}
+	}
+}
+
 func main() {
-	items := []list.Item{
-		item{title: "Raspberry Pi’s", desc: "I have ’em all over my house"},
-		item{title: "Nutella", desc: "It's good on toast"},
-		item{title: "Bitter melon", desc: "It cools you down"},
-		item{title: "Nice socks", desc: "And by that I mean socks without holes"},
-		item{title: "Eight hours of sleep", desc: "I had this once"},
-		item{title: "Cats", desc: "Usually"},
-		item{title: "Plantasia, the album", desc: "My plants love it too"},
-		item{title: "Pour over coffee", desc: "It takes forever to make though"},
-		item{title: "VR", desc: "Virtual reality...what is there to say?"},
-		item{title: "Noguchi Lamps", desc: "Such pleasing organic forms"},
-		item{title: "Linux", desc: "Pretty much the best OS"},
-		item{title: "Business school", desc: "Just kidding"},
-		item{title: "Pottery", desc: "Wet clay is a great feeling"},
-		item{title: "Shampoo", desc: "Nothing like clean hair"},
-		item{title: "Table tennis", desc: "It’s surprisingly exhausting"},
-		item{title: "Milk crates", desc: "Great for packing in your extra stuff"},
-		item{title: "Afternoon tea", desc: "Especially the tea sandwich part"},
-		item{title: "Stickers", desc: "The thicker the vinyl the better"},
-		item{title: "20° Weather", desc: "Celsius, not Fahrenheit"},
-		item{title: "Warm light", desc: "Like around 2700 Kelvin"},
-		item{title: "The vernal equinox", desc: "The autumnal equinox is pretty good too"},
-		item{title: "Gaffer’s tape", desc: "Basically sticky fabric"},
-		item{title: "Terrycloth", desc: "In other words, towel fabric"},
+	out, err := exec.Command("pw-dump").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var nodes []Node
+
+	err = json.Unmarshal(out, &nodes)
+	if err != nil {
+		log.Fatal(err)
 	}
 
+	items := []list.Item{
+		item{title: "Volume", desc: "System and playback volume"},
+		item{title: "Output", desc: "Output Devices"},
+		item{title: "Input", desc: "Input Devices"},
+	}
+
+	// for _, node := range nodes {
+	// 	if node.Type == "PipeWire:Interface:Node" &&
+	// 		(node.Info.Props.Media_class == "Audio/Sink" || node.Info.Props.Media_class == "Audio/Source") {
+	// 		items = append(items, item{
+	// 			title: node.Info.Props.Node_nick,
+	// 			desc:  node.Info.Props.Media_class,
+	// 		})
+	// 	}
+	// }
+
 	m := model{list: list.New(items, list.NewDefaultDelegate(), 0, 0)}
-	m.list.Title = "My Fave Things"
+	m.list.Title = "Audio"
 
 	p := tea.NewProgram(m, tea.WithAltScreen())
 
